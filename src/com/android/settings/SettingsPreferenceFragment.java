3fd70ebddb223cd9425b6311bdd781134017f01d
kufikugel <kufikugel@googlemail.com>
2012-12-13 23:11:13 +0100

    LCD Changer take 2
    Inspired by git fetch git://github.com/SlimRoms/packages_apps_Settings.git jb4.2 && git cherry-pick 156b17a7397afd8ef98d40fef0f90c7cc5babbce
    
    Hint: You need busybox for this
    
    Change-Id: I7b22749b2ebb14a4d4fdeb944522097beee55705
    Signed-off-by: DHO <DHO@dho.im>
    
    Update to LCD changer
    
    PS1: Shit-ton of fixes
    PS2: Small amount of clean up
    
    Change-Id: I4f367a2aaed37ae22c639ffffff16706c134ab5f
    Signed-off-by: DHO <DHO@dho.im>
    
    Conflicts:
    	res/layout/alert_dialog_text_entry.xml
    	res/values/arrays.xml
    	res/values/strings.xml
    	res/xml/display_settings.xml
    	res/xml/user_interface_settings.xml
    	src/com/android/settings/SettingsPreferenceFragment.java
    	src/com/android/settings/util/Helpers.java
    	src/com/android/settings/util/ShortcutPickerHelper.java

--- src/com/android/settings/SettingsPreferenceFragment.java
+++ src/com/android/settings/SettingsPreferenceFragment.java
@@ -49,8 +49,12 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
 
     private String mHelpUrl;
 
+<<<<<<< HEAD
     // Cache the content resolver for async callbacks
     private ContentResolver mContentResolver;
+=======
+    protected Context mContext;
+>>>>>>> 7a2761f... LCD Changer take 2
 
     @Override
     public void onCreate(Bundle icicle) {
